7c76dd34c67a1ea0a9c5d4b4c7b123ddf5d77405
VishalGowda26 <vishalygowda@gmail.com>
2025-01-02 16:19:01 +0530

    implemented databinding to components for better functionalities

--- src/app/circle/circle.component.ts
+++ src/app/circle/circle.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-circle',
   templateUrl: './circle.component.html',
-  styleUrls: ['./circle.component.css']
+  styleUrls: ['./circle.component.css'],
 })
 export class CircleComponent {
+  radius: number = 0;
+  Area: number = 0;
+  Perimeter: number = 0;
 
+  circleArea() {
+    this.Area = Math.PI * this.radius * this.radius;
+    this.Area = Math.ceil(this.Area * 100) / 100;
+    console.log(this.Area);
+  }
+
+  circlePeri() {
+    this.Perimeter = 2 * Math.PI * this.radius;
+    this.Perimeter = Math.ceil(this.Perimeter * 100) / 100;
+    console.log(this.Perimeter);
+  }
 }
--- src/app/rectangle/rectangle.component.ts
+++ src/app/rectangle/rectangle.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-rectangle',
   templateUrl: './rectangle.component.html',
-  styleUrls: ['./rectangle.component.css']
+  styleUrls: ['./rectangle.component.css'],
 })
 export class RectangleComponent {
+  result: number = 0;
+  length: number = 0;
+  width: number = 0;
 
+  calculateArea() {
+    this.result = this.length * this.width;
+    console.log(this.result);
+  }
+  calculatePerimeter() {
+    this.result = 2 * (this.length + this.width);
+    console.log(this.result);
+  }
 }

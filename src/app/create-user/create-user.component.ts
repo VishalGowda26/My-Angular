0ab08be221f194711550ac033616e553cd8b665a
VishalGowda26 <vishalygowda@gmail.com>
2025-01-21 12:00:28 +0530

    implemented nested form-group to create-user component

--- src/app/create-user/create-user.component.ts
+++ src/app/create-user/create-user.component.ts
@@ -4,18 +4,23 @@ import { FormControl, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-create-user',
   templateUrl: './create-user.component.html',
-  styleUrls: ['./create-user.component.css']
+  styleUrls: ['./create-user.component.css'],
 })
 export class CreateUserComponent {
+  userForm: FormGroup = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+    mobile: new FormControl(''),
+    address: new FormGroup({
+      village: new FormControl(),
+      district: new FormControl(),
+      state: new FormControl(),
+      pincode: new FormControl(),
+    }),
+  });
 
-  userForm:FormGroup=new FormGroup({
-    name:new FormControl(''),
-    email:new FormControl(''),
-    password:new FormControl(''),
-    mobile:new FormControl(''),
-  })
-
-  create(){
+  create() {
     console.log(this.userForm.value);
   }
 }

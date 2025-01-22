4a7981096e26a27621edae04c4ba5291f758b95e
VishalGowda26 <vishalygowda@gmail.com>
2025-01-22 19:25:04 +0530

    implement validations and navigation to login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { LoginService } from '../login.service';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  constructor(private _loginService: LoginService, private _router: Router) {}
 
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl(),
+    password: new FormControl(),
+  });
+
+  login() {
+    console.log(this.loginForm.value);
+    this._loginService.login(this.loginForm.value).subscribe(
+      (data: any) => {
+        console.log(data);
+        alert('Login Successfully!');
+        // Navigate to Url
+        this._router.navigateByUrl('/dashboard');
+      },
+      (err: any) => {
+        // console.log(err);
+        alert('Invalid Credentials!');
+      }
+    );
+  }
 }

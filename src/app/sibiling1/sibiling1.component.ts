dec5803cc3434c5cb768478f42a2f63c3b174b4b
vishalgowda26 <vishalygowda@gmail.com>
2025-01-29 10:09:44 +0530

    implemented a text area and restricted the input value

--- src/app/sibiling1/sibiling1.component.ts
+++ src/app/sibiling1/sibiling1.component.ts
@@ -9,8 +9,28 @@ import { CommonService } from '../common.service';
 export class Sibiling1Component {
   count: number = 0;
   constructor(private _commonService: CommonService) {}
-  
+
   send() {
     this._commonService.countSub.next(this.count);
   }
+
+  text: string = '';
+  textCount: number = 0;
+  maxChars: number = 50;
+  displaytext: string = 'You have reached Max character limit';
+
+  onTextChange(event: any) {
+    this.text = event.target.value;
+    this.textCount = this.text.length;
+    if (this.textCount == this.maxChars) {
+      alert('You have reached maximum characters');
+    } else {
+      return;
+    }
+  }
+
+  isMaxCharsReached(): boolean {
+    return this.textCount >= this.maxChars;
+  }
+
 }

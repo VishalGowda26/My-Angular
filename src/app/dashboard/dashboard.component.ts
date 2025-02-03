268fd0666d1961cd06755cf89ff91512ed8b6326
vishalgowda26 <vishalygowda@gmail.com>
2025-02-03 20:54:25 +0530

    Implemented auth-guard for security

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent {
-
+  constructor(private _router: Router) {}
+  logout() {
+    sessionStorage.removeItem('token');
+    alert('Logout Successfully!');
+    this._router.navigateByUrl('/login');
+  }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { LoginService } from '../login.service';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl(),
+    password: new FormControl(),
+  });
 
+  constructor(private _loginService: LoginService, private _router: Router) {}
+
+  token: string = '';
+  login() {
+    console.log(this.loginForm.value);
+    this._loginService.login(this.loginForm.value).subscribe(
+      (data: any) => {
+        this.token = data.token;
+        alert('Login Successfully!');
+        sessionStorage.setItem('token', this.token);
+        console.log(this.token);
+        this._router.navigateByUrl('/dashboard');
+      },
+      (err: any) => {
+        alert('Invalid credentials!');
+      }
+    );
+  }
 }
